9a770f2f9cd3bf8eadc881f4877bd325062758f1
ANIKET RITAM <140789442+Aashwin11@users.noreply.github.com>
2025-07-15 10:31:19 +0000

    Major changes in the instance

--- terraform/modules/asg_spot_sim/main.tf
+++ terraform/modules/asg_spot_sim/main.tf
@@ -1,51 +1,51 @@
 resource "aws_launch_template" "spot_sim" {
   name_prefix   = "${var.name_prefix}-spot-sim"
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [var.instance_sg_id]
-  user_data = base64encode(file("${path.module}/ubuntu-user-data.sh"))
+    image_id      = var.ami_id
+      instance_type = var.instance_type
+        vpc_security_group_ids = [var.instance_sg_id]
+          user_data = base64encode(file("${path.module}/ubuntu-user-data.sh"))
 
-    monitoring {
-    enabled = true
-  }
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "acting_spot_instance"
-      Role = "acting_spot"
-    }
-  }
-}
+              monitoring {
+                  enabled = true
+                    }
+                      tag_specifications {
+                          resource_type = "instance"
+                              tags = {
+                                    Name = "acting_spot_instance"
+                                          Role = "acting_spot"
+                                              }
+                                                }
+                                                }
 
-resource "aws_autoscaling_group" "spot_sim_asg" {
-  name                      = "${var.name_prefix}-spot-sim-asg"
-  max_size                  = var.max_size
-  min_size                  = var.min_size
-  desired_capacity          = var.desired_capacity
-  vpc_zone_identifier       = var.subnet_ids
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 90
-      instance_warmup        = 300
-    }
-  }
-  launch_template {
-    id      = aws_launch_template.spot_sim.id
-    version = "$Latest"
-  }
-  target_group_arns         = [var.target_group_arn]
-  health_check_type         = "ELB"
-  health_check_grace_period = 300
- tag {
-    key                 = "Name"
-    value               = "acting_spot_instance"
-    propagate_at_launch = true
-  }
+                                                resource "aws_autoscaling_group" "spot_sim_asg" {
+                                                  name                      = "${var.name_prefix}-spot-sim-asg"
+                                                    max_size                  = var.max_size
+                                                      min_size                  = var.min_size
+                                                        desired_capacity          = var.desired_capacity
+                                                          vpc_zone_identifier       = var.subnet_ids
+                                                            instance_refresh {
+                                                                strategy = "Rolling"
+                                                                    preferences {
+                                                                          min_healthy_percentage = 90
+                                                                                instance_warmup        = 300
+                                                                                    }
+                                                                                      }
+                                                                                        launch_template {
+                                                                                            id      = aws_launch_template.spot_sim.id
+                                                                                                version = "$Latest"
+                                                                                                  }
+                                                                                                    target_group_arns         = [var.target_group_arn]
+                                                                                                      health_check_type         = "ELB"
+                                                                                                        health_check_grace_period = 300
+                                                                                                         tag {
+                                                                                                             key                 = "Name"
+                                                                                                                 value               = "acting_spot_instance"
+                                                                                                                     propagate_at_launch = true
+                                                                                                                       }
 
-  tag {
-    key                 = "Role"
-    value               = "acting_spot"
-    propagate_at_launch = true
-  }
-}
+                                                                                                                         tag {
+                                                                                                                             key                 = "Role"
+                                                                                                                                 value               = "acting_spot"
+                                                                                                                                     propagate_at_launch = true
+                                                                                                                                       }
+                                                                                                                                       }
